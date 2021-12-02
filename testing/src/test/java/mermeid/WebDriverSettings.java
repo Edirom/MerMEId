6b21bc34d4af3bf5c6905b11d6a04c76e91b9077
Anastasia Wawilow <apetkau@upb.de>
2021-12-02 14:52:19 +0100

    updated to junit 5 added  Testorder added checkWorkTabPopupInputText()
    updated to junit 5
    added  Testorder
    added checkWorkTabPopupInputText()

--- testing/src/test/java/mermeid/WebDriverSettings.java
+++ testing/src/test/java/mermeid/WebDriverSettings.java
@@ -1,14 +1,13 @@
 package mermeid;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverSettings {
     public ChromeDriver driver;
-    @Before
+    @BeforeEach
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -22,7 +21,7 @@ public class WebDriverSettings {
 
     }
 
-    @After
+    @AfterEach
     public void close(){
         if (driver != null) {
             driver.quit();
