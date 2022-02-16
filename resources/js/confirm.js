3c9279754718156f5559a3e80e9cf86137188c16
Peter Stadler <stadlerpeter@yahoo.fr>
2022-02-16 21:45:09 +0100

    try to rewrite the submit handler in a generic way
    This is unfinished work!

--- resources/js/confirm.js
+++ resources/js/confirm.js
@@ -23,14 +23,46 @@ function filename_prompt(formid, text, published)
     }
 }
 
-function filecopy(ev) {
-    ev.preventDefault();
-    var source = ev.target[0].value, target, overwrite=false;
-    target = prompt("Copy " + source + " to:", source.substring(0, source.length -4) + "-copy.xml");
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "../data/copy", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("source="+source+"&target="+target+"&overwrite="+overwrite);
+
+/*
+ * callback for the copy task
+ */
+function copyprompt(params) {
+    var source = params.get("source"),
+        target = prompt("Copy " + source + " to:", source.substring(0, source.length -4) + "-copy.xml");
+    params.append('target', target);
 }
 
-document.querySelectorAll(".copyform").forEach(el => {el.addEventListener("submit", filecopy)});
+/*
+ * main event handler for the forms on the main page
+ */
+function ajaxform(ev) {
+    ev.preventDefault();
+    var source = ev.target[0].value,
+        endpoint = new URL(ev.target.getAttribute('action')),
+        method = ev.target.getAttribute('method'),
+        callback = ev.target.querySelector('[name=callback]').value, 
+        params = new URLSearchParams(endpoint.search);
+    // append all input name-value pairs (e.g. <input type="hidden" name="callback" value="copyprompt"/>) as URL parameters 
+    ev.target.querySelectorAll('input').forEach(
+        function(a,b) {
+            params.append(a.getAttribute('name'), a.getAttribute('value'))
+        }
+    )
+    // check if a callback function is given
+    if(eval("typeof " + callback) === 'function') {
+        window[callback](params)
+    }
+    //console.log(params.toString());
+    const xhttp = new XMLHttpRequest();
+    xhttp.open(method, endpoint, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.send(params.toString());
+}
+
+/*
+ * Generic form submit event handler for the main page
+ * to capture clicks on e.g. "copy", or "rename" buttons  
+ */
+document.querySelectorAll(".ajaxform").forEach(el => {el.addEventListener("submit", ajaxform)});
